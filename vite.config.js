61d922b5ef34e433ea623d2c45ae6e3997929ea2
marcelo <marksrod1990@gmail.com>
2025-09-02 20:05:58 -0300

    nada

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://bakiuru.github.io/FrontEnd-Maru"
+  plugins: [react()]
 })
